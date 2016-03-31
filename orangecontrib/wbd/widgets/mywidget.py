2d7f014e857bf1f9148371557e689e7f4eb15fcb
Miha Zidar <zidarsk8@gmail.com>
2016-03-31 03:57:07 +0200

    Fill basic country data

--- orangecontrib/wbd/widgets/mywidget.py
+++ orangecontrib/wbd/widgets/mywidget.py
@@ -1,7 +1,7 @@
 import sys
 import signal
+import wbpy
 
-from PyQt4 import QtCore
 from PyQt4 import QtGui
 from Orange.widgets.widget import OWWidget
 from Orange.widgets import gui
@@ -36,14 +36,41 @@ class CountryListWidget(QtGui.QWidget):
         filter_label = QtGui.QLabel("Filter")
         filter_text = QtGui.QLineEdit()
         filter_button = QtGui.QPushButton("Ok")
-        country_list = QtGui.QTableWidget()
-        layout.addWidget(filter_label,0,0)
-        layout.addWidget(filter_text,0,1)
-        layout.addWidget(filter_button,0,2)
-        layout.addWidget(country_list,1,0,1,3)
-
+        country_list = CountryTableWidget()
+        layout.addWidget(filter_label, 0, 0)
+        layout.addWidget(filter_text, 0, 1)
+        layout.addWidget(filter_button, 0, 2)
+        layout.addWidget(country_list, 1, 0, 1, 3)
 
         self.setLayout(layout)
+
+
+class CountryTableWidget(QtGui.QTableWidget):
+
+    def __init__(self):
+        super().__init__()
+        api = wbpy.IndicatorAPI()
+        self.countries = api.get_countries()
+        self.setColumnCount(3)
+        self.filter_data()
+
+
+    def filter_data(self, filter_string=None):
+        self.draw_items()
+
+    def draw_items(self, countries=None):
+        if countries is None:
+            countries = self.countries
+
+        self.setRowCount(len(countries))
+
+        for index, data in enumerate(countries):
+            self.setItem(index, 0,QtGui.QTableWidgetItem(data))
+            self.setItem(index, 1,QtGui.QTableWidgetItem(countries[data]["name"]))
+
+
+
+
 
 
 if __name__ == "__main__":
