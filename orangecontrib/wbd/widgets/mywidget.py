01028ade926a60b398fad0b16729e37f99c29549
Miha Zidar <zidarsk8@gmail.com>
2016-04-02 02:14:48 +0200

    Add generic Filtered table widget
    This is an attempt to combine the code for country and indicator
    filters.

--- orangecontrib/wbd/widgets/mywidget.py
+++ orangecontrib/wbd/widgets/mywidget.py
@@ -34,7 +34,11 @@ class WorldBankDataWidget(OWWidget):
         button = QtGui.QPushButton("Fetch Data")
         button.clicked.connect(self.fetch_button_clicked)
 
-        self.countries = CountryFilterWidget()
+        self.countries = Country2Widget(data=[
+            {"A": "1", "bb": "something"},
+            {"A": "2", "bb": "something"},
+            {"A": "4", "bb": "something"},
+        ])
         self.indicators = IndicatorFilterWidget()
         self.data_widget = DataTableWidget()
         self.date = DateInputWidget()
@@ -126,13 +130,13 @@ class DataTableWidget(QtGui.QTableWidget):
 
 class FilteredTableWidget(QtGui.QWidget):
 
-    def __init__(self, table_widget_class):
+    def __init__(self, table_widget_class, **kwargs):
         super().__init__()
 
         layout = QtGui.QGridLayout()
 
         self.filter_widget = simple_filter_widget.SimpleFilterWidget()
-        self.table_widget = table_widget_class()
+        self.table_widget = table_widget_class(**kwargs)
         layout.addWidget(self.filter_widget)
         layout.addWidget(self.table_widget)
 
@@ -154,8 +158,79 @@ class IndicatorFilterWidget(FilteredTableWidget):
 
 class CountryFilterWidget(FilteredTableWidget):
 
-    def __init__(self):
-        super().__init__(country_table_widget.CountryTableWidget)
+    def __init__(self, data=None):
+        super().__init__(country_table_widget.CountryTableWidget, data)
+
+
+class Country2Widget(FilteredTableWidget):
+
+    def __init__(self, data=None):
+        super().__init__(FilterDataTableWidget, data=data)
+
+
+class FilterDataTableWidget(QtGui.QTableWidget):
+
+    def __init__(self, data=None):
+        """Init data table widget.
+
+        Args:
+            data (list): List of dicts where each key in the dict represents a
+                column in the table. All dicts must contain all the same keys.
+        """
+        super().__init__()
+        self.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+        self.filtered_data = {}
+        self.data = data
+        self.filter_data()
+
+    def filter_data(self, filter_str=""):
+        """Filter data with a string and refresh the table.
+
+        Args:
+            filter_str (str): String for filtering rows of data.
+        """
+        if filter_str:
+            filtered_list = [
+                item for item in self.data if
+                any(filter_str in value for value in item.values())
+            ]
+        else:
+            filtered_list = self.data
+
+        self.draw_items(filtered_list)
+
+    def draw_items(self, data=None):
+        if data is None:
+            data = self.data
+        if not data:
+            return
+
+        headers = self._set_column_headers(data[0])
+        header_index = {key:index for index, key in enumerate(headers)}
+
+        self.filtered_data = data
+        self.setRowCount(len(data))
+        for index, data in enumerate(data):
+            for key, value in data.items():
+                self.setItem(
+                    index,
+                    header_index[key],
+                    QtGui.QTableWidgetItem(value),
+                )
+
+    def _set_column_headers(self, element):
+        """Set column count and header text.
+
+        Args:
+            element (dict): Dictionary containing single element.
+
+        Returns:
+            List of strings containing the order of headers.
+        """
+        self.setColumnCount(len(element.keys()))
+        headers = list(element.keys())
+        self.setHorizontalHeaderLabels(headers)
+        return headers
 
 
 if __name__ == "__main__":
