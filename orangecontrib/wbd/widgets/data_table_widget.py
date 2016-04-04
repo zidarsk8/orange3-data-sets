cfd2d71b2c0293034ba28c4032ec0eb3024e93d9
Miha Zidar <zidarsk8@gmail.com>
2016-04-04 03:19:23 +0200

    Update data_table_widget
    The widget itself should not do any data manipulation and all the code
    from this widget needs to be moved to wbpy API.

--- orangecontrib/wbd/widgets/data_table_widget.py
+++ orangecontrib/wbd/widgets/data_table_widget.py
@@ -13,45 +13,21 @@ class DataTableWidget(QtGui.QTableWidget):
     def __init__(self):
         super().__init__()
 
-    @classmethod
-    def _get_unique_dates(cls, dataset):
-        """Return a list of all dates found in the dataset.
-
-        This is used when there is data missing for some countries and so we
-        get a full list of dates.
-        Note; maybe it would be better to replace this with range and take into
-        account the quarters and months so that if there is a year missing in
-        all countries, that the dataset would still contain those lines"""
-        date_sets = [set(value.keys()) for value in dataset.values()]
-        return sorted(set().union(*date_sets))
-
-    def fill_data(self, dataset):
+    def fill_data(self, data):
         """Fill the main data table.
 
         Args:
-          dataset (wbpy.IndicatorDataset): indicator dataset fetch from the
-              wbpy api.
-
+            data (list of lists): 2d array where the first column represents
+            the data header.
         """
-        data_dict = dataset.as_dict()
-        dates = self._get_unique_dates(data_dict)
-        self.setRowCount(len(dates))
-        self.setColumnCount(len(data_dict))
-        date_indexes = {date: index for index, date in enumerate(dates)}
-        sorted_countries = sorted(dataset.countries.values())
-        country_index = {country: index for index, country in
-                         enumerate(sorted_countries)}
-
-        for country_id, data in data_dict.items():
-            column = country_index[dataset.countries[country_id]]
-            for date, value in data.items():
-                self.setItem(
-                    date_indexes[date],
-                    column,
-                    QtGui.QTableWidgetItem(str(value))
-                )
-        self.setHorizontalHeaderLabels(sorted_countries)
-        self.setVerticalHeaderLabels(dates)
-
-    def transpose(self):
-        """Transpose the displayed world bank data results."""
+        if not data or not data[0]:
+            self.setRowCount(0)
+            self.setColumnCount(0)
+            return
+        header = data.pop(0)
+        self.setRowCount(len(data))
+        self.setColumnCount(len(header))
+        self.setHorizontalHeaderLabels(header)
+        for row, row_data in enumerate(data):
+            for column, value in enumerate(row_data):
+                self.setItem(row, column, QtGui.QTableWidgetItem(str(value)))
