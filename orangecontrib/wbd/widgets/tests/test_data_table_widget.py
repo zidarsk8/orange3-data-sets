781808b229297ef667a41e4a2a233c43f56213dc
Miha Zidar <zidarsk8@gmail.com>
2016-04-04 03:23:11 +0200

    Add tests for data table widget

--- orangecontrib/wbd/widgets/tests/test_data_table_widget.py
+++ orangecontrib/wbd/widgets/tests/test_data_table_widget.py
@@ -1,18 +1,41 @@
-import sys
-import unittest
-import mock
+"""Test for data table widget."""
 
-from PyQt4 import QtGui
-from PyQt4 import QtTest
-from PyQt4 import QtCore
+import unittest
 
 from orangecontrib.wbd.widgets import data_table_widget
 
 
 class TestSimpleFilterWidget(unittest.TestCase):
+    """Test for data table widget."""
 
     def setUp(self):
         self.widget = data_table_widget.DataTableWidget()
+        self.data = [
+            [" ", "A", "B", "C"],
+            ["1", "x", "y", "z"],
+            ["2", "c", "a", "b"],
+            ["3", "w", "@", "+"],
+        ]
 
     def test_fill_data(self):
-        pass
+        """Test setting data from normal array.
+
+        Missing tests for headers
+        """
+        self.widget.fill_data(self.data)
+        self.assertEqual(self.widget.item(0, 0).text(), "1")
+        self.assertEqual(self.widget.item(1, 0).text(), "2")
+        self.assertEqual(self.widget.item(2, 2).text(), "@")
+        self.assertIsNone(self.widget.item(3, 0))
+
+    def test_empty_data(self):
+        """Test that data gets removed when filling with empty array."""
+        self.widget.fill_data(self.data)
+        self.assertIsNotNone(self.widget.item(0, 0))
+        self.widget.fill_data([[]])
+        self.assertIsNone(self.widget.item(0, 0))
+
+        self.widget.fill_data(self.data)
+        self.assertIsNotNone(self.widget.item(0, 0))
+        self.widget.fill_data([])
+        self.assertIsNone(self.widget.item(0, 0))
