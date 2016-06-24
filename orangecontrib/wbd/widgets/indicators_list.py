ea33e49c0254a40f3e24c9ea74a2d681fb8d40b8
Miha Zidar <zidarsk8@gmail.com>
2016-06-24 02:46:15 +0200

    Make indicator list return all indicators
    We should on return just one indicator but all of them and have the API
    handle the rest.

--- orangecontrib/wbd/widgets/indicators_list.py
+++ orangecontrib/wbd/widgets/indicators_list.py
@@ -36,10 +36,12 @@ class IndicatorsListWidget(QtGui.QWidget):
         self._fetch_data()
 
     def _init_listeners(self):
+        """Initialize the widget event listeners."""
         self.indicators.table_widget.on(
             "selection_changed", self._selection_changed)
 
     def _init_layout(self):
+        """Initialize the widget layout."""
         layout = QtGui.QGridLayout()
 
         self.indicators = filter_table.FilterTable()
@@ -64,6 +66,12 @@ class IndicatorsListWidget(QtGui.QWidget):
         self.setLayout(layout)
 
     def _fetch_data(self, filter_=None):
+        """Dispatch a fetch data request in a background thread.
+
+        Args:
+            filter_ (str): Either "common" or "featured" string to filter the
+                indicator list. Anything else will be interpreted as "all".
+        """
         self.filter_ = filter_
         self._executor = concurrent.ThreadExecutor(
             threadPool=QtCore.QThreadPool(maxThreadCount=2)
@@ -74,10 +82,16 @@ class IndicatorsListWidget(QtGui.QWidget):
         self._executor.submit(self._task)
 
     def _radio_selected(self, button):
+        """Radio button change handler.
+
+        Args:
+            button (QRadioButton): The currently selected radio button.
+        """
         logger.debug("%s radio button selected", button.text())
         self._fetch_data(button.text())
 
     def _fetch_indicators_data(self):
+        """Background thread handler for fetching data from the api."""
         logger.debug("Fetch indicator data")
         self.data = self.api.get_indicator_list(filter_=self.filter_)
 
@@ -85,11 +99,17 @@ class IndicatorsListWidget(QtGui.QWidget):
         logger.error("Failed to load indicator list.")
 
     def _fetch_indicators_completed(self):
+        """Handler for successfully completed fetch request."""
         logger.debug("Fetch indicator completed.")
         if self.data:
             self.indicators.table_widget.set_data(self.data)
 
     def _set_title(self, title=""):
+        """Set the title of the parent qtoolbox.
+
+        Args:
+            title (str): String containing a list of all selected indicators.
+        """
         if callable(self.text_setter):
             logger.debug("setting indicator widget title")
             self.text_setter(self.TITLE_TEMPLATE.format(title))
@@ -111,8 +131,10 @@ class IndicatorsListWidget(QtGui.QWidget):
         else:
             self._set_title()
 
-    def get_indicator(self):
-        selected = self.indicators.get_selected_data()
-        if selected:
-            return selected[0]
-        return None
+    def get_indicators(self):
+        """Get the list of selected indicators.
+
+        Returs:
+            list(str): List of selected indicator ids.
+        """
+        return self.indicators.get_selected_data()
