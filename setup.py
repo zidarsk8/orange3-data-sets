9a5ccb8fb1b362f675af7359c2100d45a796a2b9
Miha Zidar <zidarsk8@gmail.com>
2016-07-25 01:29:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,9 @@ ENTRY_POINTS = {
 KEYWORDS = (
     # [PyPi](https://pypi.python.org) packages with keyword "orange3 add-on"
     # can be installed using the Orange Add-on Manager
-    'orange3 add-on',
+    "orange3 add-on",
+    "World bank data",
+    "Indicator api",
 )
 
 
@@ -37,30 +39,32 @@ def get_description():
 
 if __name__ == '__main__':
     setup(
-        name="World Bank Data",
-        version="0.2.0",
+        name="Orange3-Datasets",
+        version="0.1.0",
         license="MIT",
         author="Miha Zidar",
         author_email="zidarsk8@gmail.com",
-        description=("A simple python interface for World Bank Data Indicator "
-                     "and Climate APIs"),
+        description=("Orange interface for World Bank Data Indicator and "
+                     "Climate APIs"),
         long_description=get_description(),
-        url="https://github.com/zidarsk8/world_bank_data",
-        download_url=("https://github.com/zidarsk8/world_bank_data/tarball/"
-                      "0.2.0"),
+        url="https://github.com/zidarsk8/orange3-datasets",
+        download_url=("https://github.com/zidarsk8/orange3-datasets/tarball/"
+                      "0.1.0"),
         packages=[
             'orangecontrib',
             'orangecontrib.wbd',
             'orangecontrib.wbd.tutorials',
-            'orangecontrib.wbd.widgets'
+            'orangecontrib.wbd.widgets',
         ],
         package_data={
             'orangecontrib.wbd': ['tutorials/*.ows'],
             'orangecontrib.wbd.widgets': ['icons/*'],
         },
         install_requires=[
+            'Orange3',
+            'numpy',
             'observable',
-            'simple_wbd',
+            'simple_wbd>=0.4.1',
         ],
         extras_require={
             'doc': [
@@ -73,4 +77,17 @@ if __name__ == '__main__':
         namespace_packages=['orangecontrib'],
         include_package_data=True,
         zip_safe=False,
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: X11 Applications :: Qt',
+            'Environment :: Plugins',
+            'Programming Language :: Python',
+            'Operating System :: OS Independent',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'Topic :: Scientific/Engineering :: Visualization',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Developers',
+        ],
     )
